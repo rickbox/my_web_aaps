d1f0aca52ac3b0ab2b15730a514edd7bcb1f3d01
rickbox <36268103+rickbox@users.noreply.github.com>
2018-12-26 03:32:37 +0900

    不要ファイル削除

--- books_app/app/controllers/application_controller.rb
+++ books_app/app/controllers/application_controller.rb
@@ -2,11 +2,11 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   def after_sign_in_path_for(resource)
-    "/books"
+    books_url
   end
   
   def after_sign_out_path_for(resource)
-    "/books"
+    books_url
   end
 
 end
