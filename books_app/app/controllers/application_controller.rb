89e0b6011443184b2002fdc6498bdb2b3a1b0747
rickbox <36268103+rickbox@users.noreply.github.com>
2018-12-29 20:33:22 +0900

    devise追加

--- books_app/app/controllers/application_controller.rb
+++ books_app/app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+
+  def after_sign_in_path_for(resource)
+    books_url
+  end
+  
+  def after_sign_out_path_for(resource)
+    books_url
+  end
+
 end
